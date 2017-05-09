6e04b64c83489518065187ed6ae7d5f03861c686
darik <darik@DESKTOP-6AL23QR.mshome.net>
2017-05-09 18:22:03 -0500

    Agregado el Log in, Registro y cambio de datos

--- src/lista_amigos/Lista_Amigos.java
+++ src/lista_amigos/Lista_Amigos.java
@@ -16,8 +16,8 @@ public class Lista_Amigos {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-                Principal x = new Principal();
-   x.setVisible(true);
+                Login x = new Login();
+                x.setVisible(true);
     }
     
 }
